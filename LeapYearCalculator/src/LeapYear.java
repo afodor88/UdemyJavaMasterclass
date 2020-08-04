1533eef00d5f79a03e058b0366839bf3617ac4d3
alexandrufodor <alexandru.fodor@uipath.com>
2020-08-04 20:37:18 +0300

    added

---
+++ LeapYearCalculator/src/LeapYear.java
@@ -0,0 +1,20 @@
+public class LeapYear {
+    public static boolean isLeapYear(int year) {
+        if((year < 1 || year > 9999)) {
+            return false;
+        }
+
+        if(year % 4 == 0) {
+            if(year % 100 == 0) {
+                if(year % 400 == 0) {
+                    return true;
+                }
+                return false;
+            }
+
+            return false;
+        } 
+
+        return false;
+    }
+}

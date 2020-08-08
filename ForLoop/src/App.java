8f5a5e23fa7e9e24e672d220bdc5a40582e57181
alexandrufodor <alexandru.fodor@uipath.com>
2020-08-08 22:17:06 +0300

    adde

--- ForLoop/src/App.java
+++ ForLoop/src/App.java
@@ -14,6 +14,33 @@ public class App {
             System.out
                     .println("10,000 at " + i + "% interest = " + String.format("%.2f", calculateInterest(10000.0, i)));
         }
+
+        int count = 0;
+        for (int i = 10; i < 50; i++) {
+            if (isPrime(i)) {
+                count++;
+                System.out.println("Number " + i + " is a prime number");
+                if(count == 3){
+                    break;
+                }
+            }
+        }
+
+        System.out.println("There are " + count + " prime numbers");
+    }
+
+    public static boolean isPrime(int n) {
+        if (n == 1) {
+            return false;
+        }
+
+        for (int i = 2; i <= n / 2; i++) {
+            if(n% i == 0){
+                return false;
+            }
+        }
+
+        return true;
     }
 
     public static double calculateInterest(double amount, double interestRate) {

c5c35a8f020dd143098c305e84f2b1109ad7bf35
alexandrufodor <alexandru.fodor@uipath.com>
2020-08-10 16:17:00 +0300

    added

--- FlourPacker/src/App.java
+++ FlourPacker/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println(FlourPacker.canPack(1, 0, 4));
+        System.out.println(FlourPacker.canPack(2, 2, 11));
     }
 }
--- FlourPacker/src/FlourPacker.java
+++ FlourPacker/src/FlourPacker.java
@@ -1,19 +1,11 @@
 public class FlourPacker {
     public static boolean canPack(int bigCount, int smallCount, int goal) {
-        
-        if ((bigCount < 0) || (smallCount < 0) || (goal < 0)) {
-            return false;
+        for (int i = 0; i <= bigCount; i++) {
+            for (int j = 0; j <= smallCount; j++) {
+                if ((5 * i) + j == goal)
+                    return true;
+            }
         }
-
-        int bigFlourBag = bigCount * 5;
-        int smallFlourBag = smallCount * 1;
-
-        if((bigFlourBag + smallFlourBag) >= goal){
-            return true;
-        }
-
         return false;
-
-
     }
 }

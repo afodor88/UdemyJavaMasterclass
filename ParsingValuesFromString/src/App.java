8a941d8e71d9a37335f7448d8a525f52dccae27d
alexandrufodor <alexandru.fodor@uipath.com>
2020-08-10 14:36:21 +0300

    added

---
+++ ParsingValuesFromString/src/App.java
@@ -0,0 +1,19 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String numberAsString = "2018.125";
+        System.out.println("numberAsString = " + numberAsString);
+
+        double number = Double.parseDouble(numberAsString);
+
+        System.out.println("number = " + number);
+
+        numberAsString += 1;
+        number += 1;
+
+        System.out.println("numberAsString = " + numberAsString);
+        System.out.println("number = " + number);
+
+
+
+    }
+}

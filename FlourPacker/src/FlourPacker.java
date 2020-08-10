c3fac65daa54ba045f44dfa7a7dea610eeb0555c
alexandrufodor <alexandru.fodor@uipath.com>
2020-08-10 16:19:48 +0300

    added

--- FlourPacker/src/FlourPacker.java
+++ FlourPacker/src/FlourPacker.java
@@ -9,3 +9,20 @@ public class FlourPacker {
         return false;
     }
 }
+
+
+// public class FlourPacker {
+//     public static boolean canPack(int bigCount,int smallCount, int goal){
+//      if(bigCount<0 ||smallCount<0 ||goal<0){ // you CANNOT have negative kilos of flour
+//          return false;
+//      } else{
+//         int sum=(bigCount*5)+smallCount; // your sum is number of 5 kilo bags+ number of 1 kilo bags. 
+//         if(sum<goal){ // if the sum is less than the goal return false. 
+//             return false;
+//         }  else if(goal%5<=smallCount){ // if the remaining number of small count bags is less than small count return true.
+//             return true;
+//         }
+//         return false; // else return false. 
+//      }
+//     }
+//  }

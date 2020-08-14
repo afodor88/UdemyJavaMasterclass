5ff2b3ad0c10e64ec7ead09d8a0d0e0cc38d2edd
alexandrufodor <alexandru.fodor@uipath.com>
2020-08-14 16:04:32 +0300

    added

---
+++ InputCalculator/src/InputCalculator.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class InputCalculator {
+    public static void inputThenPrintSumAndAverage(){
+        Scanner scanner = new Scanner(System.in);
+
+        int sum = 0;
+        long avg = 0;
+        int nrCount = 0;
+
+        while(true){
+            boolean isAnInt = scanner.hasNextInt();
+
+            if(isAnInt){
+                int number = scanner.nextInt();
+                sum += number;
+                nrCount++;
+            } else {
+                break;
+            }
+            scanner.nextLine();
+        }
+        
+        avg =  Math.round(((double)sum / (double )nrCount));
+        System.out.println("SUM = " + sum + " AVG = " + avg);
+        scanner.close();
+    }
+}

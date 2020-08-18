2429abcdc45bbf8156937d847d1b0cc52a31d882
alexandrufodor <alexandru.fodor@uipath.com>
2020-08-18 16:35:12 +0300

    added

---
+++ InheritanceChallenge/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Outlander outlander = new Outlander(36);
+        outlander.steer(45);
+        outlander.accelerate(30);
+        outlander.accelerate(20);
+        outlander.accelerate(-42);
+    }
+}

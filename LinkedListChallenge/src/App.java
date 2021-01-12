b924afb75e5a5e931f4d6f335f4e57f772dc44d3
alexandrufodor <alexandru.fodor@uipath.com>
2021-01-12 22:34:32 +0200

    finished linked list challenge

--- LinkedListChallenge/src/App.java
+++ LinkedListChallenge/src/App.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.ListIterator;
 import java.util.Scanner;
@@ -58,6 +59,7 @@ public class App {
             return;
         } else {
             System.out.println("Now playing " + listIterator.next().toString());
+            printMenu();
         }
 
         while (!quit) {
@@ -98,14 +100,57 @@ public class App {
                     }
                     break;
                 case 3:
+                    if (forward) {
+                        if (listIterator.hasPrevious()) {
+                            System.out.println("Now replaying " + listIterator.previous().toString());
+                            forward = false;
+                        } else {
+                            System.out.println("We are at the start of the list");
+                        }
+                    } else {
+                        if (listIterator.hasNext()) {
+                            System.out.println("Now replaying " + listIterator.next().toString());
+                            forward = true;
+                        } else {
+                            System.out.println("We have reached the end of the list");
+                        }
+                    }
                     break;
                 case 4:
-                    // printList(playList);
+                    printList(playList);
                     break;
                 case 5:
-                    // printMenu();
+                    printMenu();
+                    break;
+                case 6:
+                    if(playList.size() > 0){
+                        listIterator.remove();
+                        if(listIterator.hasNext()){
+                            System.out.println("Now playing " + listIterator.next());
+                        } else if(listIterator.hasPrevious()){
+                            System.out.println("Now playing " + listIterator.previous());
+                        }
+                    }
                     break;
             }
         }
     }
+
+    public static void printMenu() {
+        System.out.println("Available actions:\npress");
+        System.out.println("0 - to quit\n" + "1 - to play next song\n" + "2 - to play previous song\n"
+                + "3 - to replay the current song\n" + "4 - list songs in the playlist\n"
+                + "5 - print available actions\n"
+                + "6 - delete current song from playlist");
+    }
+
+    private static void printList(LinkedList<Song> playList) {
+        Iterator<Song> iterator = playList.iterator();
+        System.out.println("===========================");
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+        System.out.println("===========================");
+    }
+
 }

f08d5c040d91fd090e5affd76ada22c44caec0bf
alexandrufodor <alexandru.fodor@uipath.com>
2021-02-05 21:49:22 +0200

    added

--- InnerClasses/src/App.java
+++ InnerClasses/src/App.java
@@ -1,7 +1,61 @@
+import java.util.Scanner;
+
 public class App {
+
+    private static Scanner scanner = new Scanner(System.in);
+    private static Button btnPrint = new Button("Print");
+
     public static void main(String[] args) throws Exception {
-        GearBox mcLaren = new GearBox(6);
-        GearBox.Gear first = mcLaren.new Gear(1, 12.3);
-        System.out.println(first.driveSpeed(1000));
+
+        // class ClickListener implements Button.OnClickListener {
+        // public ClickListener() {
+        // System.out.println("I've been attached");
+        // }
+
+        // @Override
+        // public void onClick(String title) {
+        // System.out.println(title + " was clicked");
+        // }
+        // }
+
+        // btnPrint.setOnClickListener(new ClickListener());
+
+        btnPrint.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(String title) {
+                System.out.println(title + " was clicked");
+            }
+        });
+        listen();
+
+        // GearBox mcLaren = new GearBox(6);
+
+        // mcLaren.operateClutch(true);
+        // mcLaren.changeGear(1);
+        // mcLaren.operateClutch(false);
+        // System.out.println(mcLaren.wheelSpeed(1000));
+        // mcLaren.changeGear(2);
+        // System.out.println(mcLaren.wheelSpeed(3000));
+        // mcLaren.operateClutch(true);
+        // mcLaren.changeGear(3);
+        // mcLaren.operateClutch(false);
+        // System.out.println(mcLaren.wheelSpeed(6000));
+
+    }
+
+    private static void listen() {
+        boolean quit = false;
+        while (!quit) {
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+            switch (choice) {
+                case 0:
+                    quit = true;
+                    break;
+                case 1:
+                    btnPrint.onClick();
+
+            }
+        }
     }
 }

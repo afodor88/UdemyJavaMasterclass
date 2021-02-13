3a93bb9e99a159bdc5b76da7545a7ed81c001507
alexandrufodor <alexandru.fodor@uipath.com>
2021-02-13 21:54:09 +0200

    Fixing bug in MyLinkedList

--- AbstractClassChallenge/src/App.java
+++ AbstractClassChallenge/src/App.java
@@ -1,14 +1,30 @@
+
 public class App {
     public static void main(String[] args) throws Exception {
 
         MyLinkedList list = new MyLinkedList(null);
         list.traverse(list.getRoot());
-        String stringData = "Darwin Brisbane Perth Melbourne Canberra Adelaide Sydney Canberra";
+        // String stringData = "Darwin Brisbane Perth Melbourne Canberra Adelaide Sydney Canberra";
+        String stringData = "5 7 3 9 8 2 1 0 4 6";
         String[] data = stringData.split(" ");
         for (String s : data){
             list.addItem(new Node(s));
         }
 
         list.traverse(list.getRoot());
+        list.removeItem(new Node("3"));
+        list.traverse(list.getRoot());
+
+        list.removeItem(new Node("5"));
+        list.traverse(list.getRoot());
+        
+        list.removeItem(new Node("0"));
+        list.removeItem(new Node("4"));
+        list.removeItem(new Node("2"));
+        list.traverse(list.getRoot());
+
+
+
+
     }
 }

2f92458b6b95809af2499c8b00c0bd36043f0496
alexandrufodor <alexandru.fodor@uipath.com>
2021-03-23 21:32:53 +0200

    added

---
+++ PackageDemo/src/com/example/game/Monster.java
@@ -0,0 +1,54 @@
+package com.example.game;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Monster implements ISaveable {
+    private String name;
+    private int hitPoints;
+    private int strength;
+
+    public Monster(String name, int hitPoints, int strength) {
+        this.name = name;
+        this.hitPoints = hitPoints;
+        this.strength = strength;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getHitPoints() {
+        return this.hitPoints;
+    }
+
+    public int getStrength() {
+        return this.strength;
+    }
+
+    @Override
+    public List<String> write() {
+        ArrayList<String> values = new ArrayList<String>();
+        values.add(0, this.name);
+        values.add(1, "" + this.hitPoints);
+        values.add(2, "" + this.strength);
+        return values;
+    }
+
+    @Override
+    public void read(List<String> savedValues) {
+        if(savedValues != null && savedValues.size() > 0){
+            this.name = savedValues.get(0);
+            this.hitPoints = Integer.parseInt(savedValues.get(1));
+            this.hitPoints = Integer.parseInt(savedValues.get(2));
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return "{" + " name='" + getName() + "'" + ", hitPoints='" + getHitPoints() + "'" + ", strength='"
+                + getStrength() + "'" + "}";
+    }
+
+}

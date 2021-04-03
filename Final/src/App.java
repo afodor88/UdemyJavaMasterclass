81822fc58eb89a3206794faafcf9cd5414dde10a
alexandrufodor <alexandru.fodor@uipath.com>
2021-04-03 21:58:51 +0300

    added

---
+++ Final/src/App.java
@@ -0,0 +1,30 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // SomeClass one = new SomeClass("one");
+        // SomeClass two = new SomeClass("two");
+        // SomeClass three = new SomeClass("three");
+
+        // System.out.println(one.getInstanceNumber());
+        // System.out.println(two.getInstanceNumber());
+        // System.out.println(three.getInstanceNumber());
+
+        // System.out.println(Math.PI);
+        
+        // int pw = 674312;
+        // Password password = new ExtendedPassword(pw);
+        // password.storePassword();
+
+        // password.letMeIn(1);
+        // password.letMeIn(523266);
+        // password.letMeIn(9773);
+        // password.letMeIn(0);
+        // password.letMeIn(-1);
+        // password.letMeIn(674312);
+
+
+        System.out.println("Main method called");
+        SIBTest test = new SIBTest();
+        test.someMethod();
+        System.out.println("Owner is " + SIBTest.owner);
+    }
+}
---
+++ Final/src/Password.java
@@ -0,0 +1,28 @@
+public class Password {
+    private static final int key = 748576362;
+    private final int encryptedPassword;
+
+    public Password(int password) {
+        this.encryptedPassword = encryptDecrypt(password);
+    }
+
+    private int encryptDecrypt(int password) {
+        return password ^ key;
+    }
+
+    public final void storePassword() {
+        System.out.println("Saving password as " + this.encryptedPassword);
+    }
+
+    public boolean letMeIn(int password) {
+        if (encryptDecrypt(password) == this.encryptedPassword) {
+            System.out.println("Welcome");
+        } else {
+            System.out.println("Nope, you cannot come in");
+        }
+        return false;
+    }
+
+    
+
+}

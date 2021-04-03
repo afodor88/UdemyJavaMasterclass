1093a822530a5d2b3857f1b6f3596891ce47845a
alexandrufodor <alexandru.fodor@uipath.com>
2021-04-03 17:31:00 +0300

    added

---
+++ ScopeChallenge/src/com/example/App.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        X x = new X(new Scanner(System.in));
+        x.x();
+    }
+}
+

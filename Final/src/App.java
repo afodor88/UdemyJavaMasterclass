379ffbc7a36f88913facbc0ed50a7daa5123db00
alexandrufodor <alexandru.fodor@uipath.com>
2021-04-12 22:55:39 +0300

    added

--- Final/src/App.java
+++ Final/src/App.java
@@ -1,25 +1,25 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        // SomeClass one = new SomeClass("one");
-        // SomeClass two = new SomeClass("two");
-        // SomeClass three = new SomeClass("three");
+        SomeClass one = new SomeClass("one");
+        SomeClass two = new SomeClass("two");
+        SomeClass three = new SomeClass("three");
 
-        // System.out.println(one.getInstanceNumber());
-        // System.out.println(two.getInstanceNumber());
-        // System.out.println(three.getInstanceNumber());
+        System.out.println(one.getInstanceNumber());
+        System.out.println(two.getInstanceNumber());
+        System.out.println(three.getInstanceNumber());
 
-        // System.out.println(Math.PI);
+        System.out.println(Math.PI);
         
-        // int pw = 674312;
-        // Password password = new ExtendedPassword(pw);
-        // password.storePassword();
+        int pw = 674312;
+        Password password = new ExtendedPassword(pw);
+        password.storePassword();
 
-        // password.letMeIn(1);
-        // password.letMeIn(523266);
-        // password.letMeIn(9773);
-        // password.letMeIn(0);
-        // password.letMeIn(-1);
-        // password.letMeIn(674312);
+        password.letMeIn(1);
+        password.letMeIn(523266);
+        password.letMeIn(9773);
+        password.letMeIn(0);
+        password.letMeIn(-1);
+        password.letMeIn(674312);
 
 
         System.out.println("Main method called");

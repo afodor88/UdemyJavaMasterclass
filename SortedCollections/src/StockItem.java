58102c072770e0ebc3c07811b2597f64cfab9381
alexandrufodor <alexandru.fodor@uipath.com>
2021-06-22 19:54:21 +0300

    added

--- SortedCollections/src/StockItem.java
+++ SortedCollections/src/StockItem.java
@@ -1,7 +1,8 @@
 public class StockItem implements Comparable<StockItem> {
     private final String name;
     private double price;
-    private int quantityStock;
+    private int quantityInStock = 0;
+    private int reserved = 0;
 
     public StockItem(String name, double price) {
         // this.name = name;
@@ -13,15 +14,15 @@ public class StockItem implements Comparable<StockItem> {
     public StockItem(String name, double price, int quantityStock) {
         this.name = name;
         this.price = price;
-        this.quantityStock = quantityStock;
+        this.quantityInStock = quantityStock;
     }
 
     public String getName() {
         return this.name;
     }
 
-    public int quantityInStock() {
-        return this.quantityStock;
+    public int availableQuantity() {
+        return this.quantityInStock - reserved;
     }
 
     public double getPrice() {
@@ -35,10 +36,35 @@ public class StockItem implements Comparable<StockItem> {
     }
 
     public void adjustStock(int quantity) {
-        int newQuantity = this.quantityStock + quantity;
+        int newQuantity = this.quantityInStock + quantity;
         if (newQuantity >= 0) {
-            this.quantityStock = newQuantity;
+            this.quantityInStock = newQuantity;
         }
+    }
+
+    public int reserveStock(int quantity) {
+        if (quantity <= availableQuantity()) {
+            reserved += quantity;
+            return quantity;
+        }
+        return 0;
+    }
+
+    public int unreserveStock(int quantity) {
+        if (quantity <= reserved) {
+            reserved -= quantity;
+            return quantity;
+        }
+        return 0;
+    }
+
+    public int finaliseStock(int quantity) {
+        if (quantity <= reserved) {
+            quantityInStock -= quantity;
+            reserved -= quantity;
+            return quantity;
+        }
+        return 0;
     }
 
     @Override
@@ -65,21 +91,19 @@ public class StockItem implements Comparable<StockItem> {
     @Override
     public int compareTo(StockItem o) {
         System.out.println("Entering StockItem.compareTo");
-        if(this == o){
+        if (this == o) {
             return 0;
         }
 
-        if(o != null){
+        if (o != null) {
             return this.name.compareTo(o.getName());
         }
         throw new NullPointerException();
     }
 
-
     @Override
     public String toString() {
-        return this.name + " : price " + this.price;
+        return this.name + " : price " + this.price + ". Reserved: " + this.reserved;
     }
-
 
 }
